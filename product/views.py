2bbb5d5fb1db4b3ec2ebaba08895dbc3d5ef4a68
Christian-Coria <desbloqueoenlinea@gmail.com>
2022-12-31 22:11:03 -0300

    snippets

--- product/views.py
+++ product/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.db.models import Q
+from vistaprevia.models import Producto
 
-# Create your views here.
+
+
+def index(request):
+    params = {}
+    params['nombre_sitio'] = 'EnLinea'
+    producto=Producto.objects.filter( Q(estado="Publicado"), )
+    params['producto']=producto
+    print(producto)
+    return render(request, 'web/index.html', params)
